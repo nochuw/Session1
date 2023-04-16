e52c30130f269f1799816405c8a57e43c5c0a89e
Julia <julia181557@gmail.com>
2023-04-16 20:53:39 +0300

    изменено

--- GitLabApp/Program.cs
+++ GitLabApp/Program.cs
@@ -10,10 +10,11 @@ namespace GitLabApp
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите значение параметра х: ");
             string s = Console.ReadLine();
             double x = double.Parse(s);
             double F = (4 * Math.Sin(x)) * (5 * Math.Cos(x));
-            Console.WriteLine(x + " " + F);
+            Console.WriteLine($"Значение F = {F}. ");
             Console.ReadKey();
             return;
         }
